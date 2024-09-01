28341a7f951d9602e0238382b9a90cf9bc7ddcec
宋灵均 <lingjun.song@yahoo.com>
2024-08-31 21:39:00 -0400

    revised README learning

--- README.md
+++ README.md
@@ -35,3 +35,8 @@ The easiest way to deploy your Next.js app is to use the [Vercel Platform](https
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
 # practice-aws-with-S3-and-cloudfront
+
+## Learning from this project
+- Used S3 to host video and CloudFront for CDN distribution
+- Used Next JS and react for front end. 
+
--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,12 @@ import {useState, useEffect} from 'react'
 export default function Home() {
   
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between ">
       
           <h1 className="mb-3 text-2xl font-semibold">
             Welcome to MockTube
           </h1>
-          
-        
-      
-
-      <video width={800} height={500} controls loop autoPlay>
+      <video width={500} height={300} controls loop autoPlay>
         <source 
         src="https://d20a5mf2t5y2xm.cloudfront.net/practice_aws-video.MOV" type="video/mp4"
         />
