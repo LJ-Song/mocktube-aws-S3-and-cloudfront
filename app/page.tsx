572afbb8910d2460393568a532ad19ab7c120a56
宋灵均 <lingjun.song@yahoo.com>
2024-08-31 22:09:52 -0400

    changed UI

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,27 @@ export default function Home() {
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
-      
-          <h1 className="mb-3 text-2xl font-semibold">
+      <h1 className="mb-3 text-2xl font-semibold">
             Welcome to MockTube
           </h1>
-      <video width={500} height={300} controls loop autoPlay>
+      <div className="flex-row">
+      
+      
+        <h2 className="mb-3 font-semibold">
+          A Musical Day at Newbury Street
+        </h2>
+      <video width={400} height={200} controls>
         <source 
         src="https://d20a5mf2t5y2xm.cloudfront.net/practice_aws-video.MOV" type="video/mp4"
         />
       </video>
+      
+      
+      </div>
+          
+
+
+      
     </main>
   );
 }
